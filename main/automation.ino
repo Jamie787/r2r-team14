08bf48d0580557d483dc8215020a47bf75da3f0a
nagararya <nagararya@gmail.com>
2024-11-10 15:18:56 +1100

    moveThroughMaze, senseSurroundings and traverseStairs written

--- main/automation.ino
+++ main/automation.ino
@@ -1,6 +1,7 @@
 
-void moveMaze() {
+void moveThroughMaze() {
     // TO DO: adjust depending on the maze
+    // eg.
     moveDistance(MAZE_DISTANCE, f);
     moveDistance(TURNING_DISTANCE, r);
     moveDistance(MAZE_DISTANCE, f);
@@ -12,9 +13,33 @@ void moveMaze() {
     moveDistance(TURNING_DISTANCE, r);
 
     // TO DO: go back through the maze
+    // eg.
     moveDistance(MAZE_DISTANCE, f);
     moveDistance(TURNING_DISTANCE, r);
     moveDistance(MAZE_DISTANCE, f);
     moveDistance(TURNING_DISTANCE, l);
     moveDistance(MAZE_DISTANCE, f);
+};
+
+void senseSurroundings() {
+    for (int i = 0; i < 4; i++) {
+        // check surroundings
+        checkUltrasonic();
+    
+        // turn
+        moveDistance(TURNING_DISTANCE, r);
+    }
+};
+
+void traverseStairs() {
+    // go up the stairs
+    moveDistance(STAIR_DISTANCE, f);
+
+    // turn 180 degrees
+    moveDistance(TURNING_DISTANCE, r);
+    moveDistance(TURNING_DISTANCE, r);
+
+    // go down the stairs
+    moveDistance(STAIR_DISTANCE, f);
+
 };
