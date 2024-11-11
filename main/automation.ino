e39456906089b4db32ed71881c4fc555e272e758
jamie <jamiesiennagold@gmail.com>
2024-11-11 11:33:40 +1100

    changed sense command

--- main/automation.ino
+++ main/automation.ino
@@ -20,11 +20,26 @@ void moveThroughMaze() {
 };
 
 void senseSurroundings() {
-    for (int i = 0; i < 4; i++) {
-        // check surroundings and turn
-        checkUltrasonic();
-        turnDirection('r');
+  for (int i = 0; i < 4; i++) {
+    // check surroundings and turn
+    switch (i) {
+      case 0:
+        Serial.print("Forward ");
+        break;
+      case 1:
+        Serial.print("Right ");
+        break;
+      case 2:
+        Serial.print("Behind ");
+        break;
+      case 3:
+        Serial.print("Left ");
+        break;
     }
+
+    Serial.println(findSensorDistance(forwardTrig, forwardEcho));
+    turnDirection('r');
+  }
 };
 
 void traverseStairs() {
